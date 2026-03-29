547081b14aeec5e8f10ae24474a64448c3523198
bleussa <19738553+bleussa@users.noreply.github.com>
2026-03-29 11:40:21 +0200

    i18n config that i forgot to put in the last commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
